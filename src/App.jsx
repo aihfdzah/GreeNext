a486d96b0686c9b117db35970232aeef0eca6a28
aihfdzah <ainulhafidzah02@gmail.com>
2024-11-02 23:02:09 +0700

    add a css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
 import React from "react";
-import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import Content from "./components/Content";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
-      <Header />
       <Navbar />
-      <Content />
       <Footer />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <img src="./logo2.png" className="logo" alt="logo"/>
 
 
             <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar">
+                <ul className="navbar-ul">
                     <li className="nav-item">
                         <a className="nav-link" href="#dashboard">
                             Dashboard</a>
@@ -33,6 +33,10 @@ const Navbar = () => {
                             Tentang Kita</a>
                     </li>
                 </ul>
+
+                <div className="masuk">
+                    <a href="#" className="masuk-btn">Masuk</a>
+                </div>
             </div>
         </nav>
     );
