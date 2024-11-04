e45ca61666e65787af65511900ca8e16e78ba911
aihfdzah <ainulhafidzah02@gmail.com>
2024-11-04 12:25:40 +0700

    adding the content of the about page and its css, adding app.jsx

--- src/pages/Tentang.jsx
+++ src/pages/Tentang.jsx
@@ -24,22 +24,22 @@ const Tentang = () => {
                     <h3>Kenapa <span>FarmEduz</span>?</h3>
                     <div className="feature-list">
                         <div className="feature-item">
-                            <img src="path/to/certificate-icon.png" alt="Sertifikasi" class="feature-icon"/>
+                            <img src="#" alt="Sertifikasi" class="feature-icon"/>
                             <h4>Sertifikasi</h4>
                             <p>Setiap kali berhasil menyelesaikan sebuah kelas, kamu akan mendapatkan Sertifikat.</p>
                         </div>
                         <div className="feature-item">
-                            <img src="path/to/personalized-learning-icon.png" alt="Personalized Learning" class="feature-icon"/>
+                            <img src="#" alt="Personalized Learning" class="feature-icon"/>
                             <h4>Proses Pembelajaran Personalisasi</h4>
                             <p>Kamu bebas memilih spesialisasi yang diinginkan agar dapat disesuaikan dengan kebutuhan saat ini.</p>
                         </div>
                         <div className="feature-item">
-                            <img src="path/to/video-learning-icon.png" alt="Pembelajaran Video" class="feature-icon"/>
+                            <img src="#" alt="Pembelajaran Video" class="feature-icon"/>
                             <h4>Pembelajaran Berbasis Video</h4>
                             <p>Materi secara praktis melalui sesi video, tutorial langkah demi langkah, dan studi kasus nyata dari praktisi terbaik.</p>
                         </div>
                         <div className="feature-item">
-                            <img src="path/to/agriculture-icon.png" alt="Kebutuhan Pertanian" class="feature-icon"/>
+                            <img src="#" alt="Kebutuhan Pertanian" class="feature-icon"/>
                             <h4>Berdasarkan Kebutuhan Pertanian</h4>
                             <p>Hemat waktu dan biaya! Materi FarmEduz dibuat agar lebih mudah dimengerti dan sesuai dengan kebutuhan.</p>
                         </div>
