f8dea5e18f7281c3bdc79b5c76576e17e42fae0e
putraali <k9724aliputra@gmail.com>
2024-11-05 14:27:13 +0700

    menambahkan routes login dan register

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Kelas from "./pages/Kelas";
@@ -9,6 +11,8 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/regis" element={<Register />} />
 				<Route path="/" element={<Navbar />} />
 				<Route path="/kelas" element={<Kelas />} />
 				<Route path="/tentang" element={<Tentang />} />
