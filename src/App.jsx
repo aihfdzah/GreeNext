e77291345db6280c6f68263d77821148cf7bc832
putraali <k9724aliputra@gmail.com>
2024-11-06 06:38:35 +0700

    Menjadi route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Kelas from './pages/Kelas';
-import Tentang from './pages/Tentang';
+import Kelas from "./pages/Kelas";
+import Tentang from "./pages/Tentang";
+import { BrowserRouter, Router, Route } from "react-dom";
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Kelas />
-      <Tentang />
-      <Footer />
-    </div>
-  );
-};
+const App = () =>
+	//   {
+	//   return (
+	//     <div>
+	//       <Navbar />
+	//       <Kelas />
+	//       <Tentang />
+	//       <Footer />
+	//     </div>
+	//   );
+	// };
 
-// function App () {
-//   return (
-//     <BrowserRouter>
-//       <Router>
-//         <Route path="/kelas" element={<Kelas />} />
-//         <Route path="/tentang" element={<Tentang />} />
-//       </Router>
-//     </BrowserRouter>
-//   );
-// }
+	function App() {
+		return (
+			<BrowserRouter>
+				<Router>
+          <Route path=/>
+          <Route path="/register " element={<Register/>}/>
+					<Route path="/kelas" element={<Kelas />} />
+					<Route path="/tentang" element={<Tentang />} />
+				</Router>
+			</BrowserRouter>
+		);
+	};
 
 export default App;
