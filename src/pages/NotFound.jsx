9959739f0695aa1a4aae5d31d3a6113b9b8a3d07
putraali <k9724aliputra@gmail.com>
2024-11-07 11:47:59 +0700

    perubahan

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 export default function NotFound() {
-  return (
-    <div className='container not-found-page align-items-center h-100'>
-      <h1 className='title fs-1'>404</h1>
-      <p>Halaman yang anda cari tidak ada</p>
-      <a href="/">balik ke halaman utama</a>
-    </div>
-  )
+	return (
+		<div className="container not-found-page align-items-center h-100">
+			<h1 className="title fs-1">404</h1>
+			<p>Halaman yang anda cari tidak ada</p>
+			<a href="/">balik ke halaman utama</a>
+		</div>
+	);
 }
