3c43b149c03f7714dc8e42fdeb46ad3960af3fd6
putraali <k9724aliputra@gmail.com>
2024-11-12 09:48:12 +0700

    perubahan sedikit 2

---
+++ src/components/Detailebook.jsx
@@ -0,0 +1,69 @@
+import NavbarWeb from "./NavbarWeb";
+
+const Detailebook = () => {
+	return (
+		<>
+			{" "}
+			<NavbarWeb />
+			<div className="container">
+				<main className="book-detail">
+					<div className="back-button">
+						<button>&larr;</button>
+					</div>
+
+					<div className="content">
+						<div className="left-section">
+							<h2>Bioteknologi Pertanian</h2>
+							<div className="book-image">
+								<img src="./bioteknologi pertanian.jpg" alt="" />
+								<div className="favorite">
+									<button>&hearts;</button>
+								</div>
+							</div>
+
+							<div className="author">
+								Esna Dilli Novianto, Erma Suryanti, Indriati Husain, Fitri
+								Yulianti, Rosminah, Muhammad Abdul Aziz, Silvia Permata Sari,
+								Sri Suryaningsih Djunu, Juniaty Arruan Bulawan, Indah Apriliya,
+								Muhammad Isra, Syamsul Bahri
+							</div>
+							<div className="buttons">
+								<button className="read-button">Baca buku</button>
+								<button className="buy-button">Beli buku</button>
+							</div>
+						</div>
+
+						<div className="right-section">
+							<h3>Deskripsi Singkat</h3>
+							<p>
+								Buku <strong>Bioteknologi</strong> adalah disiplin ilmu yang
+								merupakan cabang dari biologi, yang fokus utamanya adalah pada
+								penerapan teknologi untuk memanfaatkan makhluk hidup—baik itu
+								mikroorganisme, tumbuhan, maupun hewan—dalam berbagai proses
+								yang bertujuan untuk menghasilkan produk, baik berupa barang
+								maupun jasa, yang dirancang untuk meningkatkan kualitas hidup
+								manusia secara keseluruhan. Cabang ilmu ini tidak hanya mencakup
+								aspek tradisional yang telah digunakan selama berabad-abad,
+								seperti fermentasi dan pemuliaan tanaman, tetapi juga telah
+								berkembang pesat dengan masuknya bioteknologi modern yang
+								melibatkan teknik-teknik canggih seperti rekayasa genetika,
+								kultur jaringan, dan penggunaan enzim dalam industri. integral
+								dari budaya, kepercayaan, dan kehidupan sehari-hari manusia.
+							</p>
+
+							<div className="progress-section">
+								<span>0 hal</span>
+								<div className="progress-bar">
+									<div className="progress" style={{ width: "1%" }}></div>
+								</div>
+								<span>1% selesai</span>
+							</div>
+						</div>
+					</div>
+				</main>
+			</div>
+		</>
+	);
+};
+
+export default Detailebook;
--- src/components/Kelas.jsx
+++ src/components/Kelas.jsx
@@ -1,235 +1,233 @@
-import Navbar from "./Navbar";
 const Kelas = () => {
-	const navigate = useNavigate();
-
-	const handleDaftarClick = () => {
-		navigate("/detailkls");
-	};
-
 	return (
 		<>
-		<Navbar/>
-		<div className="container" style={{ marginBottom: "20%" }}>
-			<div className="header">
+			<div className="container" style={{ marginBottom: "20%" }}>
+				<div className="header">
+					<div>
+						<button className="button-filter justify-content-center align-items-center">
+							Filter
+						</button>
+					</div>
+					<div className="search-bar display-flex background-transparent">
+						<i className="fa-solid fa-magnifying-glass"></i>
+						<input type="text" placeholder="Cari kursus..." />
+					</div>
+					<i className="fa-regular fa-heart"></i>
+					<i className="fa-solid fa-cart-shopping"></i>
+				</div>
+
 				<div>
-					<button className="button-filter justify-content-center align-items-center">
-						Filter
-					</button>
-				</div>
-				<div className="search-bar display-flex background-transparent">
-					<i className="fa-solid fa-magnifying-glass"></i>
-					<input type="text" placeholder="Cari kursus..." />
-				</div>
-				<i className="fa-regular fa-heart"></i>
-				<i className="fa-solid fa-cart-shopping"></i>
-			</div>
-
-			<div>
-				<h2 style={{ fontSize: "20pt" }}>62 Hasil Untuk "Pertanian"</h2>
-			</div>
-
-			{/* -------------- Layout Konten Utama -------- */}
-			<div className="main-content">
-				{/* ---------- Sidebar Filter --> */}
-				<div className="filter">
-					<select>
-						<option>Jenis Teknologi</option>
-						<option>IoT</option>
-						<option>Big Data</option>
-						<option>Agroekologi</option>
-					</select>
-					<select>
-						<option>Tingkat Kesulitan</option>
-						<option>Pemula</option>
-						<option>Menengah</option>
-						<option>Lanjut</option>
-					</select>
-					<select>
-						<option>Pelatihan</option>
-						<option>Gratis</option>
-						<option>Berbayar</option>
-					</select>
-					<select>
-						<option>Harga</option>
-						<option>Promo</option>
-						<option>Terpopuler</option>
-					</select>
-					<select>
-						<option>Sertifikasi</option>
-						<option>Terbaru</option>
-						<option>Terpopuler</option>
-					</select>
+					<h2 style={{ fontSize: "20pt" }}>62 Hasil Untuk "Pertanian"</h2>
 				</div>
 
-				{/* ---------------- Daftar Kursus ------------- */}
-				<div className="daftar-kursus display-flex flex-direction-column">
-					{/* <!-- Kursus 1 --> */}
-					<div className="course-item">
-						<div className="konten-course">
-							<p className="title-course">Pengantar Pertanian Modern</p>
-							<p className="deskripsi-course">
-								Kursus ini memberikan pemahaman dasar tentang pertanian modern,
-								mulai dari praktik pertanian tradisional hingga perkembangan
-								teknologi yang diterapkan ...
-								<a href="/detailkls" className="next">
-									Baca Selengkapnya
-								</a>
-							</p>
-							<div className="tag-course">
-								<span>Menengah</span>
-								<span>4.8/5</span>
-								<span>Gratis</span>
-							</div>
-						</div>
-						<div className="action-section">
-							<div className="content">
-								<img src="./foto1.jpg" alt="" />
-								<a href="/detailkls" className="button-daftar">Daftar Sekarang</a>
-							</div>
-						</div>
+				{/* -------------- Layout Konten Utama -------- */}
+				<div className="main-content">
+					{/* ---------- Sidebar Filter --> */}
+					<div className="filter">
+						<select>
+							<option>Jenis Teknologi</option>
+							<option>IoT</option>
+							<option>Big Data</option>
+							<option>Agroekologi</option>
+						</select>
+						<select>
+							<option>Tingkat Kesulitan</option>
+							<option>Pemula</option>
+							<option>Menengah</option>
+							<option>Lanjut</option>
+						</select>
+						<select>
+							<option>Pelatihan</option>
+							<option>Gratis</option>
+							<option>Berbayar</option>
+						</select>
+						<select>
+							<option>Harga</option>
+							<option>Promo</option>
+							<option>Terpopuler</option>
+						</select>
+						<select>
+							<option>Sertifikasi</option>
+							<option>Terbaru</option>
+							<option>Terpopuler</option>
+						</select>
 					</div>
 
-					{/* Add onClick to other buttons similarly */}
-					<div className="course-item">
-						<div className="konten-course">
-							<p className="title-course">
-								Pertanian Berkelanjutan dan Agreokologi
-							</p>
-							<p className="deskripsi-course">
-								Fokus pada konsep dan praktik pertanian berkelanjutan, kursus
-								ini mengajarkan tenang bagaimana meminimalkan dampak lingkungan
-								dari aktivitas pertanian ...
-								<a href="/detailkls" className="next">
-									Baca Selengkapnya
-								</a>
-							</p>
-							<div className="tag-course">
-								<span>Menengah</span>
-								<span>4.8/5</span>
-								<span>Gratis</span>
+					{/* ---------------- Daftar Kursus ------------- */}
+					<div className="daftar-kursus display-flex flex-direction-column">
+						{/* <!-- Kursus 1 --> */}
+						<div className="course-item">
+							<div className="konten-course">
+								<p className="title-course">Pengantar Pertanian Modern</p>
+								<p className="deskripsi-course">
+									Kursus ini memberikan pemahaman dasar tentang pertanian
+									modern, mulai dari praktik pertanian tradisional hingga
+									perkembangan teknologi yang diterapkan ...
+									<a href="/detailkls" className="next">
+										Baca Selengkapnya
+									</a>
+								</p>
+								<div className="tag-course">
+									<span>Menengah</span>
+									<span>4.8/5</span>
+									<span>Gratis</span>
+								</div>
+							</div>
+							<div className="action-section">
+								<div className="content">
+									<img src="./foto1.jpg" alt="" />
+									<a href="/detailkls" className="button-daftar">
+										Daftar Sekarang
+									</a>
+								</div>
 							</div>
 						</div>
-						<img src="./foto2.jpg" alt="" />
-						<a href="/detailkls" className="button-daftar">Daftar Sekarang</a>
-					</div>
 
-					<div className="course-item">
-						<div className="konten-course">
-							<p className="title-course">Teknik irigasi Cerdas</p>
-							<p className="deskripsi-course">
-								Kursus ini mengeksplorasi metode irigasi modern yang cerdas
-								seperti drip irigation, yang dapat mengoptimalkan penggunaan air
-								dalam pertanian ...
-								<a href="/detailkls" className="next">
-									Baca Selengkapnya
-								</a>
-							</p>
-							<div className="tag-course">
-								<span>Menengah</span>
-								<span>4.8/5</span>
-								<span>Gratis</span>
+						{/* Add onClick to other buttons similarly */}
+						<div className="course-item">
+							<div className="konten-course">
+								<p className="title-course">
+									Pertanian Berkelanjutan dan Agreokologi
+								</p>
+								<p className="deskripsi-course">
+									Fokus pada konsep dan praktik pertanian berkelanjutan, kursus
+									ini mengajarkan tenang bagaimana meminimalkan dampak
+									lingkungan dari aktivitas pertanian ...
+									<a href="/detailkls" className="next">
+										Baca Selengkapnya
+									</a>
+								</p>
+								<div className="tag-course">
+									<span>Menengah</span>
+									<span>4.8/5</span>
+									<span>Gratis</span>
+								</div>
 							</div>
+							<img src="./foto2.jpg" alt="" />
+							<a href="/detailkls" className="button-daftar">
+								Daftar Sekarang
+							</a>
 						</div>
-						<img src="./foto3.jpg" alt="" />
-						<a href="/detailkls" className="button-daftar">Daftar Sekarang</a>
-					</div>
 
-					<div className="course-item">
-						<div className="konten-course">
-							<p className="title-course">Konsep Panduan Pertanian Presisi</p>
-							<p className="deskripsi-course">
-								Kursus ini secara komprehensif memperkenalkan sistem panduan
-								otomatis yang penting untuk pertanian modern ini. dimulai dengan
-								gambaran umum tentang ...
-								<a href="#" className="next">
-									Baca Selengkapnya
-								</a>
-							</p>
-							<div className="tag-course">
-								<span>Menengah</span>
-								<span>4.8/5</span>
-								<span>Gratis</span>
+						<div className="course-item">
+							<div className="konten-course">
+								<p className="title-course">Teknik irigasi Cerdas</p>
+								<p className="deskripsi-course">
+									Kursus ini mengeksplorasi metode irigasi modern yang cerdas
+									seperti drip irigation, yang dapat mengoptimalkan penggunaan
+									air dalam pertanian ...
+									<a href="/detailkls" className="next">
+										Baca Selengkapnya
+									</a>
+								</p>
+								<div className="tag-course">
+									<span>Menengah</span>
+									<span>4.8/5</span>
+									<span>Gratis</span>
+								</div>
 							</div>
+							<img src="./foto3.jpg" alt="" />
+							<a href="/detailkls" className="button-daftar">
+								Daftar Sekarang
+							</a>
 						</div>
-						<img src="./foto4.jpg" alt="" />
-						<button className="button-daftar">Daftar Sekarang</button>
-					</div>
 
-					<div className="course-item">
-						<div className="konten-course">
-							<p className="title-course">
-								Pemanfaatan Teknologi IoT dalam Pertanian
-							</p>
-							<p className="deskripsi-course">
-								Kursus ini membahas bagaimana Internet of Things (Iot) daat
-								diterapkan untukmeningkatkan efisiensi dan produktivitas
-								pertanian. peserta akan belajar tentang ...
-								<a href="#" className="next">
-									Baca Selengkapnya
-								</a>
-							</p>
-							<div className="tag-course">
-								<span>Menengah</span>
-								<span>4.8/5</span>
-								<span>Gratis</span>
+						<div className="course-item">
+							<div className="konten-course">
+								<p className="title-course">Konsep Panduan Pertanian Presisi</p>
+								<p className="deskripsi-course">
+									Kursus ini secara komprehensif memperkenalkan sistem panduan
+									otomatis yang penting untuk pertanian modern ini. dimulai
+									dengan gambaran umum tentang ...
+									<a href="#" className="next">
+										Baca Selengkapnya
+									</a>
+								</p>
+								<div className="tag-course">
+									<span>Menengah</span>
+									<span>4.8/5</span>
+									<span>Gratis</span>
+								</div>
 							</div>
+							<img src="./foto4.jpg" alt="" />
+							<button className="button-daftar">Daftar Sekarang</button>
 						</div>
-						<img src="./foto5.jpg" alt="" />
-						<button className="button-daftar">Daftar Sekarang</button>
-					</div>
 
-					<div className="course-item">
-						<div className="konten-course">
-							<p className="title-course">
-								Pemanfaatan Big data dan AI untuk Pertanian
-							</p>
-							<p className="deskripsi-course">
-								Di era digital, penggunaan Big Data dan kecerdasan buatan AI
-								dapat membantu meningkatkan hasil pertanian dan mengurangi
-								risiko. Kursus ini menjelaskna ...
-								<a href="#" className="next">
-									Baca Selengkapnya
-								</a>
-							</p>
-							<div className="tag-course">
-								<span>Menengah</span>
-								<span>4.8/5</span>
-								<span>Gratis</span>
+						<div className="course-item">
+							<div className="konten-course">
+								<p className="title-course">
+									Pemanfaatan Teknologi IoT dalam Pertanian
+								</p>
+								<p className="deskripsi-course">
+									Kursus ini membahas bagaimana Internet of Things (Iot) daat
+									diterapkan untukmeningkatkan efisiensi dan produktivitas
+									pertanian. peserta akan belajar tentang ...
+									<a href="#" className="next">
+										Baca Selengkapnya
+									</a>
+								</p>
+								<div className="tag-course">
+									<span>Menengah</span>
+									<span>4.8/5</span>
+									<span>Gratis</span>
+								</div>
 							</div>
+							<img src="./foto5.jpg" alt="" />
+							<button className="button-daftar">Daftar Sekarang</button>
 						</div>
-						<img src="./foto6.jpg" alt="" />
-						<button className="button-daftar">Daftar Sekarang</button>
+
+						<div className="course-item">
+							<div className="konten-course">
+								<p className="title-course">
+									Pemanfaatan Big data dan AI untuk Pertanian
+								</p>
+								<p className="deskripsi-course">
+									Di era digital, penggunaan Big Data dan kecerdasan buatan AI
+									dapat membantu meningkatkan hasil pertanian dan mengurangi
+									risiko. Kursus ini menjelaskna ...
+									<a href="#" className="next">
+										Baca Selengkapnya
+									</a>
+								</p>
+								<div className="tag-course">
+									<span>Menengah</span>
+									<span>4.8/5</span>
+									<span>Gratis</span>
+								</div>
+							</div>
+							<img src="./foto6.jpg" alt="" />
+							<button className="button-daftar">Daftar Sekarang</button>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className="pagination">
-				<a href="#" className="prev disabled">
-					← Sebelumnya
-				</a>
-				<span className="page-numbers">
-					<a href="#" className="page active">
-						1
+				<div className="pagination">
+					<a href="#" className="prev disabled">
+						← Sebelumnya
 					</a>
-					<a href="#" className="page">
-						2
+					<span className="page-numbers">
+						<a href="#" className="page active">
+							1
+						</a>
+						<a href="#" className="page">
+							2
+						</a>
+						<a href="#" className="page">
+							3
+						</a>
+						<span className="dots">...</span>
+						<a href="#" className="page">
+							12
+						</a>
+						<a href="#" className="page">
+							13
+						</a>
+					</span>
+					<a href="#" className="next">
+						Selanjutnya →
 					</a>
-					<a href="#" className="page">
-						3
-					</a>
-					<span className="dots">...</span>
-					<a href="#" className="page">
-						12
-					</a>
-					<a href="#" className="page">
-						13
-					</a>
-				</span>
-				<a href="#" className="next">
-					Selanjutnya →
-				</a>
+				</div>
 			</div>
-		</div>
 		</>
 	);
 };
