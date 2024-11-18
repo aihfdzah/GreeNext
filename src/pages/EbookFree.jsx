5444ab9dfdb6762dc26508dcbbd452590346a5ce
aihfdzah <ainulhafidzah02@gmail.com>
2024-11-18 09:11:20 +0700

    add page ebook free (blm selesai)

---
+++ src/pages/EbookFree.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Detailebook = () => {
+    return (
+        <> <NavbarWeb />
+        <div class="container">
+            <div class="title">Pemuliaan Tanaman Modern</div>
+            
+            <div class="cover">
+                <img src="./inovasi agribisnis.jpg" alt="Book Cover"/>
+                <div class="navigation">
+                    <span class="nav-icon">&#8592;</span>
+                    <span class="nav-icon">&#8594;</span>
+                </div>
+            </div>
+
+            <div class="authors">
+                Lestari Irene Purba, Badariah, Hari Gunawan, Dewi Andriani,
+                Maghfiroh Jamil, Indriani Husain, Silva Peniata Sari,
+                Maysari Yamin, Jumiarah, Muhaiman Lubis
+            </div>
+
+            <div class="pagination">1 of 189</div>
+        </div>
+
+        </>
+    );
+};
