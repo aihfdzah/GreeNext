43029cdaac642647d948f34fda595123674b4c3d
aihfdzah <ainulhafidzah02@gmail.com>
2024-11-18 14:28:12 +0700

    change EbookFree grts

---
+++ src/pages/Ebookgrts.jsx
@@ -0,0 +1,15 @@
+// import Footer from "../components/Footer";
+// import EbookFree from "../components/EbookFree";
+// import NavbarWeb from "../components/NavbarWeb";
+
+// function EbookFree() {
+// 	return (
+// 		<>
+// 			<NavbarWeb />
+// 			<EbookFree />
+// 			<Footer />
+// 		</>
+// 	);
+// }
+
+// export default EbookFree;
