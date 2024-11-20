08fb1787945c2c98975c1c31854ef5f87c53b0da
aihfdzah <ainulhafidzah02@gmail.com>
2024-11-20 09:54:45 +0700

    chance css content detail ebook & ebook lock

--- src/pages/EbookFree.jsx
+++ src/pages/EbookFree.jsx
@@ -10,14 +10,15 @@ const EbookFree = () => {
 			<div className="container">
 				<div className="title">Pemuliaan Tanaman Modern</div>
 
+			<div className="container_inside">
 				<div className="cover">
 					<img src="./inovasi agribisnis.jpg" alt="Book Cover" />
 					<div className="navigation">
 						<button className="nav-icon">
-							<a href="/ebooklock">
+							{/* <a href="/ebooklock"></a> */}
 							
-							&#8592;</a></button>
-						<span className="nav-icon">&#8594;</span>
+							&#8592;</button>
+						<button className="nav-icon">&#8594;</button>
 					</div>
 				</div>
 
@@ -26,6 +27,8 @@ const EbookFree = () => {
 					Jamil, Indriani Husain, Silva Peniata Sari, Maysari Yamin, Jumiarah,
 					Muhaiman Lubis
 				</div>
+			</div>
+				
 
 				<div className="pagination">1 of 189</div>
 
