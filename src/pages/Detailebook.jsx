4e306bafa0f66110f6a7d4c92acbbd1575e1eea2
putraali <k9724aliputra@gmail.com>
2024-11-20 13:33:36 +0700

    detailpage

--- src/pages/Detailebook.jsx
+++ src/pages/Detailebook.jsx
@@ -1,5 +1,4 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/Navbar"; import Footer from "../components/Footer";
 
 const Detailebook = () => {
 	return (
