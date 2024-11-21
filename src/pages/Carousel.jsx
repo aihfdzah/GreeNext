e008976fc06be662855d99e3769b2b908628867d
aihfdzah <ainulhafidzah02@gmail.com>
2024-11-21 11:36:17 +0700

    perubahan carousel kesekian

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -7,14 +7,14 @@ const Carousel = () => {
 			<Navbar />
             <div className="container">
                 <section class="product"> 
-                    <h2 class="product-category">best selling</h2>
+                    <h2 class="product-category">Best Ebook</h2>
                     <button class="pre-btn"><img src="arrow.png" alt=""/></button>
                     <button class="nxt-btn"><img src="arrow.png" alt=""/></button>
                     
                     <div class="product-container">
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">FREE</span>
                                 <img src="./bioteknologi pertanian.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">open</button>
                                 <a href="/ebookfree"></a>
@@ -22,12 +22,11 @@ const Carousel = () => {
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">FREE</span>
                                 <img src="./inovasi agribisnis.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">open</button>
                                 <a href="/ebookfree"></a>
@@ -35,103 +34,102 @@ const Carousel = () => {
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">LOCK</span>
                                 <img src="./manajemen agroindustri.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">buy</button>
                             </div>
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
+                                <span class="price">$20</span><span class="actual-price">$50</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">LOCK</span>
                                 <img src="./manajemen agroindustri.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">buy</button>
                             </div>
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
+                                <span class="price">$20</span><span class="actual-price">$50</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">LOCK</span>
                                 <img src="./pertanian masa depan.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">buy</button>
                             </div>
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
+                                <span class="price">$20</span><span class="actual-price">$50</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">LOCK</span>
                                 <img src="./pertanian postmodern.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">buy</button>
                             </div>
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
+                                <span class="price">$35</span><span class="actual-price">$55</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">LOCK</span>
                                 <img src="./produktivitas tanama pertanian.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">buy</button>
                             </div>
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
+                                <span class="price">$25</span><span class="actual-price">$40</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">LOCK</span>
                                 <img src="./tanaman modern.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">buy</button>
                             </div>
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
+                                <span class="price">$40</span><span class="actual-price">$70</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">LOCK</span>
                                 <img src="./teknologi.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">buy</button>
                             </div>
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
+                                <span class="price">$45</span><span class="actual-price">$75</span>
                             </div>
                         </div>
                         <div class="product-card">
                             <div class="product-image">
-                                <span class="discount-tag">50% off</span>
+                                <span class="discount-tag">LOCK</span>
                                 <img src="./pertanian 4.0.jpg" class="product-thumb" alt=""/>
                                 <button class="card-btn">buy</button>
                             </div>
                             <div class="product-info">
                                 <h2 class="product-brand">buku</h2>
                                 <p class="product-short-description">a short line about the cloth..</p>
-                                <span class="price">$20</span><span class="actual-price">$40</span>
+                                <span class="price">$45</span><span class="actual-price">$75</span>
                             </div>
                         </div>
                     </div>
