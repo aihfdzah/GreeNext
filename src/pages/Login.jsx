58c103854cbbc4f3c3d45d90d0a388c3103f87f5
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-12-03 08:29:52 +0800

    Integration frontend backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,8 +21,9 @@ function Login() {
 	useEffect(() => {
 		const fetchUsers = async () => {
 			try {
-				const response = await axios.get("https://your-api-endpoint.com/users");
-				setUsers(response.data);
+				const response = await axios.get("http://localhost:5000/api/v1/user");
+				setUsers(response.data.data);
+				console.log(users)
 			} catch (err) {
 				console.error("Failed to fetch users:", err);
 				setError("Gagal mengambil data pengguna.");
