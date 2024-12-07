542dd9d816f916ef9690e460b08b2f9efc17d485
aihfdzah <ainulhafidzah02@gmail.com>
2024-12-07 16:18:27 +0700

    lagi lagi

--- src/components/Profilekeamanan.jsx
+++ src/components/Profilekeamanan.jsx
@@ -5,7 +5,7 @@ const Profilekeamanan = () => {
 	const navigate = useNavigate();
 
 	return (
-		<div className="containerr" id="profilekeamanan" style={{ marginBottom: "20%", marginTop: "10%" }}>
+		<div className="containerr" id="profilekeamanan" style={{ marginBottom: "20%", marginTop: "8%" }}>
 			<div className="sidebar">
 				<img src={profile} alt="Profile Picture" />
 				<h3>Wahyu Rojolele</h3>
--- src/components/Profileprivasi.jsx
+++ src/components/Profileprivasi.jsx
@@ -8,7 +8,7 @@ const Profileprivasi = () => {
 		<div
 			className="containerr"
 			id="profileprivasi"
-			style={{ marginBottom: "20%", marginTop: "10%" }}>
+			style={{ marginBottom: "20%", marginTop: "8%" }}>
 			<div className="sidebar">
 				<img src={profile} alt="Profile Picture" />
 				<h3>Wahyu Rojolele</h3>
